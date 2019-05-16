6227864687e18bb4371542f1557acefae4e68c52
nik1806 <15ucc045@lnmiit.ac.in>
2019-05-16 21:02:56 +0530

    hu feature completed

--- fire_detector.py
+++ fire_detector.py
@@ -28,7 +28,8 @@
 import cv2
 import numpy as np
 import pandas as pd
-# import pickle
+import argparse
+import pickle
 
 
 # feature extraction for SVM model
@@ -61,21 +62,7 @@ def label_contour(frame):
     print("****** {} *************".format(label))
     return ret, feature, label
 
-# # segmenting red-orange color in 'hue' of HSV color space
-# def red_region_hsv(frame):
-#     frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-#     light_orange = (1, 190, 200) 
-#     dark_orange = (18, 255, 255)
-
-#     mask_fire = cv2.inRange(frame_hsv, light_orange, dark_orange)
-
-#     result = cv2.bitwise_and(frame, frame, mask=mask_fire)
-
-#     return mask_fire
-
-# Return regions with highest R color component;
-# After histogram equilization (to reduce bad effects of illumination changes)
 def red_region_rgb(frame, gray_frame, RTH=220):
 
     b,g,r = cv2.split(frame)
@@ -91,11 +78,12 @@ def red_region_rgb(frame, gray_frame, RTH=220):
     G = g 
     R = r
 
-    Roi = [np.logical_and(np.logical_and(R > G, G > B), R > RTH)]
+    Roi = (np.logical_and(np.logical_and(R > G, G > B), R > RTH))
     res[Roi] = gray_frame[Roi]
     _, res = cv2.threshold(res, 50, 255, cv2.THRESH_BINARY)
 
     return res
+
 
 # YCrCb color space better distinguish luminance and chrominance information of the image
 def red_region_yCrCb(frame, gray_frame):
@@ -107,11 +95,12 @@ def red_region_yCrCb(frame, gray_frame):
     Cr = frame_y[:,:,1]
     Cb = frame_y[:,:,2]
 
-    Roi = [np.logical_and(Y > Cr, Cr > Cb)]
+    Roi = (np.logical_and(Y > Cr, Cr > Cb))
     res[Roi] = gray_frame[Roi]
     _, res = cv2.threshold(res, 50, 255, cv2.THRESH_BINARY)
 
     return res
+
 
 # combining the detect red regions from both color spaces
 def red_region(frame):
@@ -121,143 +110,166 @@ def red_region(frame):
 
     res2 = red_region_yCrCb(frame, gray_frame)
 
-    res = cv2.bitwise_and(res1, res2)
+    res = cv2.bitwise_or(res1, res2)
 
     return res
 
 
 
 """ main """
+if __name__ == "__main__":
 
-cap = cv2.VideoCapture('sample.mp4')
-# total_frame = cap.get(7)
-# cap.set(100, total_frame//2)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--train', type = bool, default=False, help="set if in training mode else False to test")
+    parser.add_argument('-v','--videofile', default="sample.mp4", help="path to video file")
+    arg = parser.parse_args()
 
-last_frame = -1 # no last frame
+    cl_flag = arg.train # Set if train else False to load model and predict
 
-feature_set = []
-label_set = []
-cl_flag = False # Set if train else False to load model and predict
+    filename = arg.videofile
+    cap = cv2.VideoCapture(filename)
+    # total_frame = cap.get(7)
+    # cap.set(100, total_frame//2)
 
-# filename = 'flame_model.sav'
-# if not cl_flag:
-#     model = pickle.load(open(filename, 'rb'))
+    last_frame = -1 # no last frame
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    try:
-        frame = cv2.resize(frame, (640, 480))
-    except:
-        continue
+    feature_set = []
+    label_set = []
 
-    kernel = np.ones((7, 7), dtype=np.uint8) # for morphological operations
+    filename_cls = 'flame_model.sav'
+    file_sc = 'ftr_scale.sav'
 
-    mr = 4 # pixel margin
+    if not cl_flag:
+        classifier = pickle.load(open(filename_cls, 'rb'))
+        scalar = pickle.load(open(file_sc, 'rb'))
 
-    """ Red region detection """
-    # res2 = red_region_yCrCb(frame)
-    res2 = red_region(frame)
-    res2 = cv2.morphologyEx(res2, cv2.MORPH_OPEN, kernel)
-    cv2.imshow("red region", res2)
+    while(cap.isOpened()):
+        ret_frame, frame = cap.read()
+        try:
+            frame = cv2.resize(frame, (640, 480))
+        except:
+            continue
+
+        # frame = frame[160:300, 260:420]
+
+        kernel = np.ones((7, 7), dtype=np.uint8) # for morphological operations
+
+        mr = 4 # pixel margin
+
+        """ Red region detection """
+        # res2 = red_region_yCrCb(frame)
+        res2 = red_region(frame)
+        res2 = cv2.morphologyEx(res2, cv2.MORPH_OPEN, kernel)
+        cv2.imshow("red region", res2)
 
 
-    """ Applying frame differencing(to capture the dynamic changes) """
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_frame = np.zeros(gray.shape, np.uint8)
-    # gray_frame = cv2.equalizeHist(gray_frame)
-    # gray_frame = frame[:,:,2] # motion of flames is not captured efficiently so capturing changes over R component
-    gray_frame[res2 > 1] = gray[res2 > 1]
+        """ Applying frame differencing(to capture the dynamic changes) """
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_frame = np.zeros(gray.shape, np.uint8)
+        # gray_frame = cv2.equalizeHist(gray_frame)
+        # gray_frame = frame[:,:,2] # motion of flames is not captured efficiently so capturing changes over R component
+        gray_frame[res2 > 1] = gray[res2 > 1]
 
-    dilute_frame = cv2.GaussianBlur(gray_frame, (9,9), 20) 
+        dilute_frame = cv2.GaussianBlur(gray_frame, (9,9), 20) 
 
-    if np.all(last_frame == -1): # recording the first frame
+        if np.all(last_frame == -1): # recording the first frame
+            last_frame = dilute_frame
+            continue
+
+        # absolute difference between the frame to detect the changed (moving) region
+        delta_frame = cv2.absdiff(dilute_frame, last_frame) 
+        
+        # Binarize
+        _, bin_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)
+
+        # filling holes -> closing
+        close_res = cv2.morphologyEx(bin_frame, cv2.MORPH_CLOSE, kernel)
+
+        # updating the frame
         last_frame = dilute_frame
-        continue
+        frame_copy = frame.copy()
 
-    # absolute difference between the frame to detect the changed (moving) region
-    delta_frame = cv2.absdiff(dilute_frame, last_frame) 
-    
-    # Binarize
-    _, bin_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)
-
-    # filling holes -> closing
-    close_res = cv2.morphologyEx(bin_frame, cv2.MORPH_CLOSE, kernel)
-
-    # updating the frame
-    last_frame = dilute_frame
-    frame_copy = frame.copy()
-
-    res = close_res
-    cv2.imshow("motion", res)
+        res = close_res
+        cv2.imshow("motion", res)
 
 
-    """ Combined result """
-    res_com = cv2.bitwise_and(res, res2)
-    res_com = cv2.morphologyEx(res_com, cv2.MORPH_CLOSE, kernel)
+        """ Combined result """
+        res_com = cv2.bitwise_and(res, res2)
+        res_com = cv2.morphologyEx(res_com, cv2.MORPH_CLOSE, kernel)
 
-    cnts, _ = cv2.findContours(res_com, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:3]
+        cnts, _ = cv2.findContours(res_com, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
 
-    ret = False
-    for contour in cnts: # approximating the closed curve
-        # if cv2.contourArea(contour) < 8:
-        #     continue
-
-        appr_curve = contour
-        # peri = cv2.arcLength(contour, True) # not improving performance 
-        # appr_curve = cv2.approxPolyDP(contour, 0.02 * peri, closed = True)
-        (x, y, w, h) = cv2.boundingRect(appr_curve)
-
-        lf_x = x if x-mr < 0 else x-mr
-        lf_y = y if y-mr < 0 else y-mr
-
-        if cl_flag:
-            try:
-                # extracting featurees and creating data set 
-                ret, fd, lb = label_contour(frame[lf_x:x+w+mr, lf_y:y+h+mr])
-                
-                if ret:
-                    break
-
-                # feature_set = []
-                feature_set.append(np.hstack((fd, lb)))
-                # feature_set.append(lb)
-                label_set.append(lb)
-                
-                print("label vector", label_set)
-            except:
-                continue
-        else:
-            """ 
-                The implimentation is remaining 
-                It is expected to remove false positive cases and improve the performance drastically
-            """
-            # # predicting if correct using SVM
-            # feature = fd_hu_moments(gray[lf_x:x+h+mr, lf_y:y+h+mr])
-            # feature = np.asarray(feature).reshape((1,-1))
-            # y_res = model.predict(feature)
-
-            # if y_res != 1: #fire not predicted
+        ret = False
+        for contour in cnts: # approximating the closed curve
+            # if cv2.contourArea(contour) < 8:
             #     continue
-        
-        # drawing the bounding box on ROI
-        cv2.rectangle(frame_copy, (lf_x, lf_y), (x+w+mr, y+h+mr), (0, 255, 0), 2)
-        
+
+            frame_copy = frame.copy() # freshly drawing on each curve
+
+            appr_curve = contour
+            # peri = cv2.arcLength(contour, True) # not improving performance 
+            # appr_curve = cv2.approxPolyDP(contour, 0.02 * peri, closed = True)
+            (x, y, w, h) = cv2.boundingRect(appr_curve)
+
+            lf_x = x if x-mr < 0 else x-mr
+            lf_y = y if y-mr < 0 else y-mr
+
+            
+            
+            if cl_flag: # training
+                
+                cv2.rectangle(frame_copy, (lf_x, lf_y), (x+w+mr, y+h+mr), (0, 255, 0), 2)
+                cv2.imshow("com", frame_copy)
+                try:
+                    # extracting featurees and creating data set 
+                    ret, fd, lb = label_contour(frame[lf_y:y+h+mr, lf_x:x+w+mr])
+                    
+                    if ret:
+                        break
+
+                    # feature_set = []
+                    feature_set.append(np.hstack((fd, lb)))
+                    # feature_set.append(lb)
+                    label_set.append(lb)
+                    
+                    print("label vector", label_set)
+                except:
+                    continue
+            else:
+                """ 
+                    The implimentation is remaining 
+                    It is expected to remove false positive cases and improve the performance drastically
+                """
+                # predicting if correct using SVM
+                feature = fd_hu_moments(gray[lf_y:y+h+mr, lf_x:x+w+mr])
+                feature = np.asarray(feature).reshape((1,-1))
+                feature_sc = scalar.transform(feature)
+                y_res = classifier.predict(feature_sc)
+
+                if y_res == 1: #fire not predicted
+                    cv2.rectangle(frame_copy, (lf_x, lf_y), (x+w+mr, y+h+mr), (0, 255, 0), 2)
+                    # frame_copy = frame.copy() # freshly drawing on each curve
+                    # continue
+
+            
+            if not cl_flag: # testing
+            # drawing the bounding box on ROI
+                cv2.imshow("com", frame_copy)            
 
 
-    # res_com = np.hstack((gray_frame, res_com))
-    # cv2.imshow("com", res_com)
-    cv2.imshow("com", frame_copy)
+        # res_com = np.hstack((gray_frame, res_com))
+        # cv2.imshow("com", res_com)
 
-    if cv2.waitKey(30) == ord('q') or ret:
-        # dataset = {'feature': feature_set, 'label': label_set}
-        if cl_flag: 
-            dataset = feature_set
-            df = pd.DataFrame(dataset)
-            df.to_csv('flame_fire.csv', index=False)
-        break
+        if cv2.waitKey(30) == ord('q') or not(ret_frame) or ret:
+            # dataset = {'feature': feature_set, 'label': label_set}
+            if cl_flag: 
+                dataset = feature_set
+                df = pd.DataFrame(dataset)
+                df.to_csv('flame.csv', index=False)
+            break
 
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
--- kernel_svm.py
+++ kernel_svm.py
@@ -7,23 +7,26 @@ import pandas as pd
 import pickle
 
 # Importing the dataset
-dataset = pd.read_csv('flame2.csv')
+dataset = pd.read_csv('flame_fire.csv')
 X = dataset.iloc[:, 0:7].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 10)
 
-# # Feature Scaling
-# from sklearn.preprocessing import StandardScaler
-# sc = StandardScaler()
-# X_train = sc.fit_transform(X_train)
-# X_test = sc.transform(X_test)
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+file_scalar = "ftr_scale.sav"
+pickle.dump(sc, open(file_scalar, 'wb')) # storing configuration of scaling function also
 
 # Fitting Kernel SVM to the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'poly', random_state = 10)
+classifier = SVC(kernel = 'linear')
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
